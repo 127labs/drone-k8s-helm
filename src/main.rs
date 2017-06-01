fa8845ec0d3a46af23111f2d3f547630750b9ae8
Imran Ismail <imran.codely@gmail.com>
2017-06-01 17:12:06 +0800

    [WIP] Configs and values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::env;
+
+struct Config {
+    values: Vec<Value>,
+}
+
+struct Value {
+    key: String,
+    value: String
+}
+
+impl Value {
+    fn new(key: &str, value: &str) -> Value {
+        Value { key: key.to_string(), value: value.to_string() }
+    }
+}
+
+impl Config {
+    fn new() -> Config {
+        Config { values: Vec::new() }
+    }
+
+    fn put_value(&mut self, value: Value) {
+        self.values.push(value);
+    }
+
+    fn load_values(&mut self) {
+        let mut values = env.vars.filter_map(|var| )
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut config = Config::new();
+
+    let value = Value::new("HOST", "ilinked.asia");
+
+    config.put_value(value);
+
+    for value in config.values {
+        println!("{}={}", value.key, value.value);
+    }
 }

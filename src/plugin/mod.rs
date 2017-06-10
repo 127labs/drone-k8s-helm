4d8869f77383a00639e8b59aa57110788cff6911
Faris Amali Alis <faris@starchild.my>
2017-06-11 05:42:40 +0800

    implement plugin traits for config struct

--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -5,56 +5,51 @@ use std::process::Command;
 use std::path::PathBuf;
 use std::env;
 
+use utils;
 use config::Config;
 
-fn which(exe: &str) -> Option<PathBuf> {
-    env::var_os("PATH").and_then(|paths| {
-        env::split_paths(&paths)
-            .filter_map(|dir| {
-                            let full_path = dir.join(&exe);
-                            if full_path.is_file() {
-                                Some(full_path)
-                            } else {
-                                None
-                            }
-                        })
-            .next()
-    })
+trait Plugin {
+    fn build_clean_command(&self) -> Command;
+    fn build_upgrade_command(&self) -> Command;
 }
 
-fn build_upgrade_command(config: &Config) -> Command {
-    let helm_bin = which("helm").expect("Helm to be installed");
-    let mut command = Command::new(helm_bin.to_str().unwrap());
+impl Plugin for Config {
+    fn build_clean_command(&self) -> Command {
+        let kubectl_bin = utils::which("kubectl").expect("Kubernetes CLI to be installed");
+        let mut command = Command::new(kubectl_bin.to_str().unwrap());
 
-    command
-        .arg("upgrade")
-        .arg("-i")
-        .arg(config.release.as_str().unwrap());
-
-    for (key, value) in config.values.as_object().unwrap() {
         command
-            .arg("--set")
-            .arg(format!("{}={}", key, value.as_str().unwrap()).as_str());
+            .arg("delete")
+            .arg("jobs")
+            .arg("-l")
+            .arg(format!("release={}", self.release.as_str().unwrap()));
+        command
     }
 
-    command.arg(config.chart.as_str().unwrap());
-    command
+    fn build_upgrade_command(&self) -> Command {
+        let helm_bin = utils::which("helm").expect("Helm to be installed");
+        let mut command = Command::new(helm_bin.to_str().unwrap());
+
+        command
+            .arg("upgrade")
+            .arg("-i")
+            .arg(self.release.as_str().unwrap());
+
+        for (key, value) in self.values.as_object().unwrap() {
+            command
+                .arg("--set")
+                .arg(format!("{}={}", key, value.as_str().unwrap()).as_str());
+        }
+
+        command.arg(self.chart.as_str().unwrap());
+        command
+    }
 }
 
-fn build_clean_command(config: &Config) -> Command {
-    let kubectl_bin = which("kubectl").expect("Kubernetes CLI to be installed");
-    let mut command = Command::new(kubectl_bin.to_str().unwrap());
-
-    command
-        .arg("delete")
-        .arg("jobs")
-        .arg("-l")
-        .arg(format!("release={}", config.release.as_str().unwrap()));
-    command
-}
 
 pub fn upgrade(config: &Config) -> () {
-    build_upgrade_command(config)
+    config
+        .build_upgrade_command()
         .status()
         .expect("Failed to execute helm upgrade command");
 }
@@ -63,7 +58,8 @@ pub fn clean(config: &Config) -> () {
     let clean_before_release = config.clean_before_release.as_bool().unwrap();
 
     if clean_before_release == true {
-        build_clean_command(config)
+        config
+            .build_clean_command()
             .status()
             .expect("Failed to delete jobs from master");
     }

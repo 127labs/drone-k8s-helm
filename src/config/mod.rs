a37b933145d55d46ecb232f23a5ade516f641f5f
Faris Amali Alis <faris@starchild.my>
2017-06-14 15:45:15 +0800

    Move Plugin implementation for Config into config module

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -8,10 +8,14 @@ use std::fs;
 use std::fs::File;
 use std::io::Write;
 use std::collections::BTreeMap;
+use std::process::Command;
 
 use handlebars::Handlebars;
 use serde_json::Value;
 use regex::Regex;
+
+use utils;
+use plugin::Plugin;
 
 const TEMPLATE: &'static str = "\
 apiVersion: v1
@@ -45,6 +49,39 @@ pub struct Config {
     pub token: Value,
     pub clean_before_release: Value,
     pub values: Value,
+}
+
+impl Plugin for Config {
+    fn build_clean_command(&self) -> Command {
+        let kubectl_bin = utils::which("kubectl").expect("Kubernetes CLI to be installed");
+        let mut command = Command::new(kubectl_bin.to_str().unwrap());
+
+        command
+            .arg("delete")
+            .arg("jobs")
+            .arg("-l")
+            .arg(format!("release={}", self.release.as_str().unwrap()));
+        command
+    }
+
+    fn build_upgrade_command(&self) -> Command {
+        let helm_bin = utils::which("helm").expect("Helm to be installed");
+        let mut command = Command::new(helm_bin.to_str().unwrap());
+
+        command
+            .arg("upgrade")
+            .arg("-i")
+            .arg(self.release.as_str().unwrap());
+
+        for (key, value) in self.values.as_object().unwrap() {
+            command
+                .arg("--set")
+                .arg(format!("{}={}", key, value.as_str().unwrap()).as_str());
+        }
+
+        command.arg(self.chart.as_str().unwrap());
+        command
+    }
 }
 
 impl Config {
--- src/plugin/mod.rs
+++ src/plugin/mod.rs
@@ -6,44 +6,10 @@ use std::process::Command;
 use utils;
 use config::Config;
 
-trait Plugin {
+pub trait Plugin {
     fn build_clean_command(&self) -> Command;
     fn build_upgrade_command(&self) -> Command;
 }
-
-impl Plugin for Config {
-    fn build_clean_command(&self) -> Command {
-        let kubectl_bin = utils::which("kubectl").expect("Kubernetes CLI to be installed");
-        let mut command = Command::new(kubectl_bin.to_str().unwrap());
-
-        command
-            .arg("delete")
-            .arg("jobs")
-            .arg("-l")
-            .arg(format!("release={}", self.release.as_str().unwrap()));
-        command
-    }
-
-    fn build_upgrade_command(&self) -> Command {
-        let helm_bin = utils::which("helm").expect("Helm to be installed");
-        let mut command = Command::new(helm_bin.to_str().unwrap());
-
-        command
-            .arg("upgrade")
-            .arg("-i")
-            .arg(self.release.as_str().unwrap());
-
-        for (key, value) in self.values.as_object().unwrap() {
-            command
-                .arg("--set")
-                .arg(format!("{}={}", key, value.as_str().unwrap()).as_str());
-        }
-
-        command.arg(self.chart.as_str().unwrap());
-        command
-    }
-}
-
 
 pub fn upgrade(config: &Config) -> () {
     config
